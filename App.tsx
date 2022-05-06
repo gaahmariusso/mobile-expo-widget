435e3038fcbecf2437001c63bdcf2732445a537f
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2022-05-06 12:30:16 -0300

    add Options, Option and Copyright

--- App.tsx
+++ App.tsx
@@ -1,19 +1,38 @@
+import 'react-native-gesture-handler';
+
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
 
 import { theme } from './src/theme';
-import { Widget } from './src/components/Widget/index';
+import Widget from './src/components/Widget';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={{
       flex: 1,
       backgroundColor: theme.colors.background
-    }}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    }}
+    >
+
+      <StatusBar
+        style="light"
+        backgroundColor='transparent'
+        translucent
+      />
+
+      <Widget />
     </View>
   );
 }
-
-
